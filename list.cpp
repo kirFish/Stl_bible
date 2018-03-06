c6a423dbbeb621d9ef6bc77e19683020b51e42c2
Sansara1 <32936237+Sansara1@users.noreply.github.com>
2018-03-06 19:43:20 +0200

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -37,20 +37,20 @@ int main()
 	mylist.push_back(180);											// Insert in back of List
 	mylist.push_front(380);											// Insert in front of List
 
-	for_each(ls.begin(), ls.end(), show);cout << endl;
+	for_each(mylist.begin(), mylist.end(), show);cout << endl;
 
 	list<int> mylist2;
 	mylist2.assign(5, 66);
 
-	ls.sort(greater<int>());									// Sorting on greater
+	mylist.sort(greater<int>());									// Sorting on greater
 	cout << "Sort: ";
 	for_each(mylist.begin(), mylist.end(), show);cout << endl;
   
-	ls2.merge(ls);												// Merge and sort
+	mylist2.merge(mylist);												// Merge and sort
 	cout << "Merge: ";
-	for_each(ls2.begin(), ls2.end(), show);cout << endl;
+	for_each(mylist2.begin(), mylist2.end(), show);cout << endl;
 
 	ls2.unique();												// Remove duplicate items in a group
 	cout << "Unique: ";
-	for_each(ls2.begin(), ls2.end(), show);cout << endl;
+	for_each(mylist2.begin(), mylist2.end(), show);cout << endl;
 }
